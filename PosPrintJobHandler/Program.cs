ef4f1d390606f6fe3e870b52e7e4f73baf5607bf
sadidkhan <sadid.edu@gmail.com>
2018-07-25 02:32:15 +0600

    Update : Local Print method used to print and posprint working from html page

--- PosPrintJobHandler/Program.cs
+++ PosPrintJobHandler/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Win32;
+using PrinterUtility;
 
 namespace PosPrintJobHandler
 {
@@ -12,17 +14,30 @@ namespace PosPrintJobHandler
     {
         static void Main(string[] args)
         {
-            foreach (var s in args)
+            //args = new string[] { "posprintjobhandler:77, 97, 109, 97, 32, 72, 97, 108, 105, 109, 10" };
+            if (args.Length > 0)
             {
-                Console.WriteLine(s);
+                var parameterString = args[0].ToString().Replace("posprintjobhandler:", "");
 
+                if (string.IsNullOrEmpty(parameterString)) return;
+                var values = parameterString.Split(',');
+                if (values.Length > 0)
+                {
+                    var bytes = values.Select(_ => _.Trim()).Select(_ => (byte)int.Parse(_)).ToArray();
+                    PrinterUtility.EscPosEpsonCommands.EscPosEpson obj = new PrinterUtility.EscPosEpsonCommands.EscPosEpson();
+                    Print(bytes, "\\\\Desktop-8h2b42n\\RONGTA");
+                }
             }
-            using (var client = new HttpClient())
-            {
-                var responseString = client.GetStringAsync("http://www.example.com/recepticle.aspx");
-                Console.WriteLine("it executed");
-                Console.ReadKey();
-            }
+        }
+
+        public static void Print(byte[] bytes, string port)
+        {
+            var desktopPath = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory);
+            var filePath = desktopPath + "\\PosPrint\\tmpPrint.print";
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+            File.WriteAllBytes(filePath, bytes);
+            File.Move(filePath, port);
         }
     }
 }
