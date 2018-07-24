0bbdae7e190aa05024f7a402b8060d2ec14e890a
sadidkhan <sadid.edu@gmail.com>
2018-07-24 18:26:52 +0600

    initialization

---
+++ PosPrintJobHandler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace PosPrintJobHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var s in args)
+            {
+                Console.WriteLine(s);
+
+            }
+            using (var client = new HttpClient())
+            {
+                var responseString = client.GetStringAsync("http://www.example.com/recepticle.aspx");
+                Console.WriteLine("it executed");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
